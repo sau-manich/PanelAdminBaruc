f3bb30ea3dd7572dca1478cdacec722100672787
saul-manich <saulmamani73@gmail.com>
2017-11-29 10:04:32 -0400

    Panel Admin Baruc
    Panel administrativo para peluquerias Baruc

---
+++ Baruc/js/site.js
@@ -0,0 +1,61 @@
+
+var calendars = {};
+
+$(document).ready( function() {
+
+  var thisMonth = moment().format('YYYY-MM');
+
+  var eventArray = [
+    { startDate: thisMonth + '-10', endDate: thisMonth + '-14', title: 'Multi-Day Event' },
+    { startDate: thisMonth + '-21', endDate: thisMonth + '-23', title: 'Another Multi-Day Event' }
+  ];
+  calendars.clndr1 = $('.cal1').clndr({
+    events: eventArray,
+    clickEvents: {
+      click: function(target) {
+        console.log(target);
+        if($(target.element).hasClass('inactive')) {
+          console.log('not a valid datepicker date.');
+        } else {
+          console.log('VALID datepicker date.');
+        }
+      },
+      nextMonth: function() {
+        console.log('next month.');
+      },
+      previousMonth: function() {
+        console.log('previous month.');
+      },
+      onMonthChange: function() {
+        console.log('month changed.');
+      },
+      nextYear: function() {
+        console.log('next year.');
+      },
+      previousYear: function() {
+        console.log('previous year.');
+      },
+      onYearChange: function() {
+        console.log('year changed.');
+      }
+    },
+    multiDayEvents: {
+      startDate: 'startDate',
+      endDate: 'endDate'
+    },
+    showAdjacentMonths: true,
+    adjacentDaysChangeMonth: false
+  });
+
+  $(document).keydown( function(e) {
+    if(e.keyCode == 37) {
+      calendars.clndr1.back();
+      calendars.clndr2.back();
+    }
+    if(e.keyCode == 39) {
+      calendars.clndr1.forward();
+      calendars.clndr2.forward();
+    }
+  });
+
+});
